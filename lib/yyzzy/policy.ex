229dfcb1744c2c6318c418c8167b369f9147c2db
Matt Ahrens <matthew.ahrens@tufts.edu>
2016-02-13 01:00:20 -0500

    strengthened the uniqueness of the time based policies

--- lib/yyzzy/policy.ex
+++ lib/yyzzy/policy.ex
@@ -11,8 +11,18 @@ defmodule Yyzzy.Policy do
   def overwrite(y1, y2), do: y2
   ##
   # Metric / Heuristic policies
-  def recent(y1 = %Yyzzy{metadata: %{time: t1}}, y2 = %Yyzzy{metadata: %{time: t2}}) do
+  @doc """
+  returns the entity with the biggest timestamp
+  if either don't have a time then we can't compare and the first is returned
+  """
+  def most_recent(y1 = %Yyzzy{metadata: %{time_updated: t1}},
+                  y2 = %Yyzzy{metadata: %{time_updated: t2}}) do
     if t1 > t2, do: y1, else: y2
   end
+  def most_recent(y1, y2), do: y1
+  @doc """
+  returns the entity with the biggest timestamp and shadows over its children
+  """
+  def most_recent_parent(y1, y2), do: {:halt, most_recent(y1, y2)}
 
 end
